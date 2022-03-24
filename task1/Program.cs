ee0f7034d4454718c90eeb581aa15ce29d862639
Nikita <nikitya39@gmail.com>
2022-03-24 23:09:24 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число");
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine ("Введите степень заданного числа");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1; 
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+Console.WriteLine (result);
