882df31c2ce06f04db7cc6ae64b1d47be59816da
Nikita <nikitya39@gmail.com>
2022-03-27 01:45:07 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+string numbers = Convert.ToString(Console.ReadLine());
+char[] chars = numbers.ToCharArray();
+int array = numbers.Length;
+int sum = 0;
+for (int i=0; i < numbers.Length; i++)
+{
+sum =sum + int.Parse(chars[i].ToString());
+}
+Console.WriteLine (sum);
